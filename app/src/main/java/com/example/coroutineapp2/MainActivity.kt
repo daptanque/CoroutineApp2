cb77b6b546fd049491262b918f692a06f11a449b
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-02-14 16:25:08 +0000

    Setup the coroutine
    Background Thread, Main Tread

--- app/src/main/java/com/example/coroutineapp2/MainActivity.kt
+++ app/src/main/java/com/example/coroutineapp2/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.coroutineapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.coroutineapp2.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        sayHelloFromMainThread()
+        sayHelloFromBackThread()
+
     }
+
+
+    private fun sayHelloFromMainThread() {
+        CoroutineScope(Dispatchers.Main).launch{
+            binding.text1.text = "Hello from " +
+                    " ${Thread.currentThread().name} "
+        }
+    }
+
+    private fun sayHelloFromBackThread() {
+        CoroutineScope(Dispatchers.IO).launch {
+            binding.text2.text = "Hello from " +
+                    " ${Thread.currentThread().name}"
+        }
+    }
+
 }
